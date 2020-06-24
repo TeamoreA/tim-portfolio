339508d4d6c86c07e914dd99ecd335948279678e
Timothy Kamau <teamorekamau97@gmail.com>
2020-06-25 01:35:15 +0300

    Deploy on heroku

--- details/views.py
+++ details/views.py
@@ -4,10 +4,14 @@ from .models import Project
 
 
 def index(request):
+    """
+    view for the index page
+    """
     project_list = Project.objects.order_by('-start_date')
     context = {'project_list': project_list}
     images = []
     for project in project_list:
+        # get the right image path
         img_path = "/"+'/'.join(project.image.name.split('/')[1:])
         images.append(img_path)
     project_list = zip(project_list, images)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,14 +126,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Static files (CSS, JavaScript, Images)
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-#  Add configuration for static files storage using whitenoise
+# Static files (CSS, JavaScript, Images)
+# STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+
+# Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
